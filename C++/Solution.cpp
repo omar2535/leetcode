0638b46aad17e97501e7153a71a9926fdc2d9f69
Omar Tsai <omar2535@gmail.com>
2019-10-31 18:51:32 -0700

    adding makefiles for cpp

---
+++ C++/Solution.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+	public:
+    vector<string> fizzBuzz(int n) {
+      cout << "hello from fizzBuzz" << endl;
+      cout << "new stuff" << endl;
+    }
+};
--- C++/main.cpp
+++ C++/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
-#include "fizz_buzz.cpp"
+#include "Solution.cpp"
 
 using namespace std;
 
 int main() {
-  cout << "HELLO WORLD" << endl;
-  cout << "Wrote my own make file" << endl;
-  FizzBuzz fb;
+  cout << "Initializating Leetcode Project" << endl;
+  for(int i=0; i<5; i++){
+    for(int j=0; j<10; j++) {
+      cout << "_";
+    }
+    cout << "\n";
+  }
+  Solution fb;
   fb.fizzBuzz(1);
+
+  return 0;
 }
