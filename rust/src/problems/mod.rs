e2e4b50767a6c63c669084db118f5a0254fe29f1
Omar2535 <omar2535@alumni.ubc.ca>
2022-11-01 22:47:23 -0400

    finish stack impl

--- rust/src/problems/mod.rs
+++ rust/src/problems/mod.rs
@@ -2,3 +2,4 @@ pub mod p344_reverse_string;
 pub mod p412_fizzbuzz;
 pub mod p739_daily_temperatures;
 pub mod p766_toeplitz_matrix;
+pub mod p255_implement_stack_using_queues;
---
+++ rust/src/problems/p255_implement_stack_using_queues.rs
@@ -0,0 +1,54 @@
+struct MyStack {
+    stack: Vec<i32>
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyStack {
+
+    fn new() -> Self {
+        return MyStack { stack: Vec::new() };
+    }
+    
+    fn push(&mut self, x: i32) {
+        self.stack.push(x);
+    }
+    
+    fn pop(&mut self) -> i32 {
+        let s = match self.stack.pop() {
+            Some(s) => {s},
+            None => {panic!("no")},
+        };
+        return s;
+    }
+    
+    fn top(&self) -> i32 {
+        return *self.stack.last().unwrap();
+    }
+    
+    fn empty(&self) -> bool {
+        return self.stack.len() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * let obj = MyStack::new();
+ * obj.push(x);
+ * let ret_2: i32 = obj.pop();
+ * let ret_3: i32 = obj.top();
+ * let ret_4: bool = obj.empty();
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+
+    }
+}
